db19fceea94a3a13976b3d2e3d7539eb25bf9441
Timo Heister <timo.heister@gmail.com>
2015-11-27 16:09:10 -0500

    make build using cmake

---
+++ config.h
@@ -0,0 +1,12 @@
+#define FIELDMASK_SIZE 4096
+#define BUFF_SIZE 1024
+#define VERSION "5.8.1"
+#define HAVE_STRING_H 1
+#define STDC_HEADERS 1
+
+#define EOF_INDICATOR "<<*>>"
+#define LINE_SEP  "----------------"
+#define HAVE_LONG_LONG 1
+/* Number of decimal digits used in displaying the values */
+/* for absolute and relative error.                       */
+#define DEF_LIM     10
